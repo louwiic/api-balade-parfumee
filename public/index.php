9a1db0b9358ba64da5cf61f0ba115153449b513c
loic batonnet <louwiic@gmail.com>
2023-10-09 22:44:35 +0400

    fix cors

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if ($_SERVER['APP_DEBUG']) {
+/* if ($_SERVER['APP_DEBUG']) {
     header('Access-Control-Allow-Origin:'.rtrim($_SERVER['HTTP_REFERER'], '/'));
 } else {
     header('Access-Control-Allow-Origin:https://balade-parfumee-2ydzvfltv-loics-projects-c950e0a8.vercel.app');
@@ -13,6 +13,15 @@ header('Access-Control-Allow-Headers:*');
 header('Access-Control-Allow-Credentials:true');
 header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    die();
+} */
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
     die();
 }
 
