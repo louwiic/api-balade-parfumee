90c9937d230f27c9c7923897f5ff037f6e9cf8a0
loic batonnet <louwiic@gmail.com>
2023-10-09 22:50:56 +0400

    fix cors

--- public/index.php
+++ public/index.php
@@ -16,6 +16,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     die();
 } */
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header("Allow: *");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === "OPTIONS") {
+    die();
+}
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
