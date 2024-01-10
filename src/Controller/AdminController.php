dadfbee9e7f4313d132989c09a143151580cf6d2
loic batonnet <louwiic@gmail.com>
2024-01-10 15:23:06 +0400

    add notifications users route

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -32,6 +32,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 class AdminController extends AbstractController
 {
     private string $publicDir;
+    private UserRepository $userRepository;
 
     public function __construct(
         private NormalizerInterface        $normalizer,
@@ -39,16 +40,17 @@ class AdminController extends AbstractController
         private EntityManagerInterface     $em,
         Kernel                             $kernel,
         EntityManagerInterface $entityManager,
+        UserRepository $userRepository,
         public  $monthlySubscription,
         public  $quarterlySubscription,
         public  $freeSubscription,
-    )
-    {
+
+    ) {
         $this->publicDir = $kernel->getProjectDir() . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'contentExclusive';
 
         $dotenv = new Dotenv();
         $dotenv->load($kernel->getProjectDir() . DIRECTORY_SEPARATOR . '.env');
-
+        $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
         $this->endpoint_secret = $_ENV['ENDPOINT_SECRET'];
         Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
@@ -105,9 +107,8 @@ class AdminController extends AbstractController
 
     #[Route('api/admin/addContentExclusive', name: 'app_addContentExclusive', methods: "POST")]
     public function addContentExclusive(Request $request, ValidatorInterface $validator, ContentTagRepository $contentTagRepository): JsonResponse
-    { 
-        
-        
+    {
+
         if (!$this->isGranted('ROLE_ADMIN')) {
             return new JsonResponse([
                 'message' => 'Vous n\'avez pas les droits pour effectuer cette action.'
@@ -177,11 +178,11 @@ class AdminController extends AbstractController
         $contentExclusive->setLink($contentTag->getId() === 3 ? $link : '');
 
         try {
- 
+
             $uniqueNameImage = $this->getUniqueFileName($imageSrc);
             if ($uniqueNameImage['isFailed']) {
                 return new JsonResponse(['error' => 'Impossible de générer un nom de fichier unique après 50 tentatives. Veuillez réessayer.'], 400);
-            }else{
+            } else {
                 $contentExclusive->setImageSrc($uniqueNameImage['nameFile']);
                 $imageSrc->move($this->publicDir, $uniqueNameImage['nameFile']);
                 $contentExclusive->setImageSrc($uniqueNameImage['nameFile']);
@@ -206,6 +207,107 @@ class AdminController extends AbstractController
         return new JsonResponse(['message' => 'Fichier ajouté avec succès']);
     }
 
+    #[Route('api/admin/edit/contentExclusive/{id}', name: 'app_update_ContentExclusive', methods: "POST")]
+    public function updateContentExclusive(Request $request, int $id, ValidatorInterface $validator, ContentTagRepository $contentTagRepository): JsonResponse
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return new JsonResponse([
+                'message' => 'Vous n\'avez pas les droits pour effectuer cette action.'
+            ], 403);
+        }
+        $file = $request->files->get('file');
+        $imageSrc = $request->files->get('image');
+        $title = $request->request->get('title');
+        $link = $request->request->get('link');
+        $description = $request->request->get('description');
+        $idContentTag = $request->request->get('idContentTag');
+
+        $contentTag = $contentTagRepository->find($idContentTag);
+        $contentExclusive = $this->em->getRepository(ContentExclusive::class)->find($id);
+
+        if (!$contentExclusive) {
+            return new JsonResponse(['message' => 'Contenu exclusif non trouvé.'], 404);
+        }
+
+
+        if (isset($file)) {
+            $violations = $validator->validate($file, [
+                new Assert\File([
+                    'maxSize' => '500M',
+                    'mimeTypes' => ['application/pdf', 'audio/mpeg'],
+                    'mimeTypesMessage' => 'Veuillez télécharger un fichier PDF ou MP3 valide.'
+                ])
+            ]);
+
+            if (count($violations) > 0) {
+                return $this->json(['message' => $violations[0]->getMessage()], 400);
+            }
+            $result = $this->getUniqueFileName($file);
+            if ($result['isFailed']) {
+                return new JsonResponse(['error' => 'Impossible de générer un nom de fichier unique après 50 tentatives. Veuillez réessayer.'], 400);
+            }
+
+            // Supprimez l'ancien fichier s'il existe
+            $oldFileName = $contentExclusive->getAudio();
+
+            if ($oldFileName) {
+                $oldFilePath = $this->publicDir . '/' . $oldFileName;
+                if (file_exists($oldFilePath)) {
+                    unlink($oldFilePath);
+                }
+            }
+
+            $file->move($this->publicDir, $result['nameFile']);
+            if ($contentTag->getId() === 2)
+                $contentExclusive->setAudio($result['nameFile']);
+            else
+                $contentExclusive->setDesktopPdf($result['nameFile']);
+        }
+
+        if (isset($imageSrc)) {
+            if (!$imageSrc instanceof UploadedFile) {
+                return $this->json(['message' => 'image not found']);
+            }
+
+            $violations = $validator->validate($imageSrc, [
+                new Assert\NotBlank([
+                    'message' => 'Veuillez télécharger un fichier.'
+                ]),
+                new Assert\File([
+                    'maxSize' => '500M',
+                    'mimeTypes' => ['image/png', 'image/jpeg'],
+                    'mimeTypesMessage' => 'Veuillez télécharger un fichier PNG ou JPEG valide.'
+                ])
+            ]);
+            if (count($violations) > 0) {
+                return $this->json(['message' => $violations[0]->getMessage()], 400);
+            }
+            $uniqueNameImage = $this->getUniqueFileName($imageSrc);
+            if ($uniqueNameImage['isFailed']) {
+                return new JsonResponse(['error' => 'Impossible de générer un nom de fichier unique après 50 tentatives. Veuillez réessayer.'], 400);
+            } else {
+                $contentExclusive->setImageSrc($uniqueNameImage['nameFile']);
+                $imageSrc->move($this->publicDir, $uniqueNameImage['nameFile']);
+                $contentExclusive->setImageSrc($uniqueNameImage['nameFile']);
+            }
+        }
+
+        if (isset($link)) {
+            $contentExclusive->setLink($link);
+        }
+        if (isset($title)) {
+            $contentExclusive->setTitle($title);
+        }
+
+        if (isset($description)) {
+            $contentExclusive->setDescription($description);
+        }
+
+        $this->em->flush();
+
+        return new JsonResponse(['message' => 'Mise à jour réussie']);
+    }
+
     #[Route('api/admin/contentExclusive/{contentExclusive}', name: 'app_updateContentExclusive', methods: "DELETE")]
     public function removeContentExclusive(ContentExclusive $contentExclusive): JsonResponse
     {
@@ -220,16 +322,25 @@ class AdminController extends AbstractController
     }
 
     #[Route('api/admin/getAllContentExclusive', name: 'getAllContentExclusive', methods: "get")]
-    public function getAllContentExclusive()
+    public function getAllContentExclusive(Request $request)
     {
         if (!$this->isGranted('ROLE_ADMIN')) {
             return new JsonResponse([
                 'message' => 'Vous n\'avez pas les droits pour effectuer cette action.'
             ], 403);
         }
-        $contentExclusives = $this->contentExclusiveRepository->findAll();
+
+        $page = $request->query->get('page', 1);
+        $limit = $request->query->get('limit', 15);
+        $offset = ($page - 1) * $limit;
+
+        $totalPage = ceil(count($this->contentExclusiveRepository->findAll()) / $limit);
+
+        // Utiliser les paramètres pour paginer les résultats
+        $contentExclusives = $this->contentExclusiveRepository->findBy([], null, $limit, $offset);
         $contentExclusives = $this->normalizer->normalize($contentExclusives, null, ['ignored_attributes' => ['tag']]);
-        return new JsonResponse(['contentExclusives' => $contentExclusives]);
+
+        return new JsonResponse(['contentExclusives' => $contentExclusives, 'totalPage' => $totalPage]);
     }
 
     #[Route('api/admin/getAllNotification', name: 'app_getAllNotification', methods: "GET")]
@@ -243,8 +354,36 @@ class AdminController extends AbstractController
         $notifications = $notificationRepository->findBy(['deletedAt' => null], ['createAt' => 'DESC']);
         $normalizedNotifications = $this->normalizer->normalize($notifications, null, ['ignored_attributes' => ['user'], 'groups' => 'notification:read']);
         return new JsonResponse(['notifications' => $normalizedNotifications]);
-
     }
+
+    #[Route('api/users/getAllNotification', name: 'app_getUsersAllNotification', methods: "GET")]
+    public function getAllUserNotification(NotificationRepository $notificationRepository): JsonResponse
+    {
+        $user = $this->userRepository->findOneByEmail($this->getUser()->getUserIdentifier());
+        $subscription = Subscription::retrieve($user->getIdSubscriptionStripe());
+        $currentSub = $subscription->items->data[0]->plan->id;
+        $categoryFilter = null;
+
+        switch ($currentSub) {
+            case 'price_1NwqwRFnV1sRkwn0cRKvCyLc':
+                $categoryFilter = [2];
+                break;
+
+            case 'price_1NwqvOFnV1sRkwn0yaK0jhlH':
+                $categoryFilter = [3];
+                break;
+
+            default:
+                $categoryFilter = [1, 4];
+                break;
+        }
+
+        $notifications = $notificationRepository->findBy(['categoryNotification' => $categoryFilter], ['createAt' => 'DESC']);
+        $normalizedNotifications = $this->normalizer->normalize($notifications, null, ['ignored_attributes' => ['user'], 'groups' => 'notification:read']);
+
+        return new JsonResponse(['notifications' => $normalizedNotifications]);
+    }
+
     #[Route('api/admin/deleteNotification/{notification}', name: 'app_deleteNotification', methods: "delete")]
     public function deleteNotification(Notification $notification): JsonResponse
     {
@@ -274,7 +413,7 @@ class AdminController extends AbstractController
         $jsonData = json_decode($request->getContent(), true);
         $categoryNotification = $categoryNotificationRepository->find($jsonData['categoryNotificationId']);
 
-        if(!$categoryNotification instanceof CategoryNotification){
+        if (!$categoryNotification instanceof CategoryNotification) {
             return new JsonResponse(['message' => 'Category not found'], 404);
         }
         if ($jsonData['title'] === null || $jsonData['description'] === null) {
@@ -302,20 +441,36 @@ class AdminController extends AbstractController
         return new JsonResponse(['message' => 'Notification ajoutée avec succès']);
     }
 
-    #[Route('api/admin/notification/{notification}', name: 'app_updateNotification', methods: "PUT")]
-    public function updateNotification(Notification $notification, Request $request) {
+    #[Route('api/admin/update/notification', name: 'app_updateNotification', methods: "POST")]
+    public function updateNotification(Request $request, CategoryNotificationRepository $categoryNotificationRepository)
+    {
+        $jsonData = json_decode($request->getContent(), true);
+        $notification = $this->em->getRepository(Notification::class)->find($jsonData['id']);
         if (!$this->isGranted('ROLE_ADMIN')) {
             return new JsonResponse([
                 'message' => 'Vous n\'avez pas les droits pour effectuer cette action.'
             ], 403);
         }
 
-        $jsonData = json_decode($request->getContent(), true);
         if ($jsonData['title'] === null || $jsonData['description'] === null) {
             return new JsonResponse(['message' => 'Title or description not found'], 404);
         }
+
+
         $notification->setDescription($jsonData['description']);
         $notification->setTitle($jsonData['title']);
+
+        $categoryNotification = $categoryNotificationRepository->find($jsonData['categoryNotificationId']);
+
+        if (isset($jsonData['categoryNotificationId'])) {
+            $notification->setCategoryNotification($categoryNotification);
+        }
+        /* return new JsonResponse([
+            'title' => $notification->getTitle(), 'description' => $notification->getDescription(),
+            "categ" => $notification->getCategoryNotification(),
+            "categoryNotification" => $jsonData['categoryNotificationId']
+        ]); */
+
         $this->em->flush();
         return new JsonResponse(['message' => 'Notification modifiée avec succès']);
     }
@@ -338,13 +493,13 @@ class AdminController extends AbstractController
         $totalPage = ceil(count($userRepository->findAll()) / $usersPerPage);
         $listUsers = [];
         // boucle pour récupérer les utilisateurs
-        foreach ($users as $user) {      
-            $listUsers [] = [
+        foreach ($users as $user) {
+            $listUsers[] = [
                 "id" => $user->getId(),
                 "firstName" => $user->getFirstName(),
                 "lastName" => $user->getLastName(),
                 "email" => $user->getEmail(),
-                "createdAt"=> $user->getCreatedAt(),
+                "createdAt" => $user->getCreatedAt(),
                 "isAdmin" => in_array("ROLE_ADMIN", $user->getRoles()),
             ];
         }
@@ -355,6 +510,8 @@ class AdminController extends AbstractController
             'totalPage' => $totalPage
         ]);
     }
+
+
 
     #[Route('api/admin/getTotalSubscriptions', name: 'get_all_total_subscriptions', methods: "GET")]
     public function getAllSubscriptions(): JsonResponse
@@ -383,5 +540,4 @@ class AdminController extends AbstractController
             return new JsonResponse(['error' => 'Une erreur s\'est produite lors de la récuperation des abonnements.'], 500);
         }
     }
-
 }
